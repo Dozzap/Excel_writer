42143fa4e2a72d35f3f9f4ce606c2d5b6d589614
Dhylan Usi <78162649+Dozzap@users.noreply.github.com>
2023-08-03 20:22:38 -0400

    Minor Update: Increase input variables

--- clean_data.py
+++ clean_data.py
@@ -1,8 +1,10 @@
 #This is a program that will translate the given input into a list of strings
-input1 = "S 5 5 5 5 0 5 P S P 5 P S 5 5 5 5 5 5 5 5 p 0 5 T 5 5 "
-input2 = "S " * 17 
+input1 = ""
+input2 = ""
 input3 = ""
-p = input1 + input2 + input3
+input4 = ""
+input5 = ""
+p = input1 + input2 + input3 input4 + input5
 def clean_data(input_data):
      transformed_string = input_data.replace("5", "S").replace("0", "O").upper().split()
 
@@ -15,6 +17,8 @@ print(len(clean_data(p)))
 print(len(clean_data(input1)))
 print(len(clean_data(input2)))
 print(len(clean_data(input3)))
+print(len(clean_data(input4)))
+print(len(clean_data(input5)))
 print()
 
 print(clean_data(p))
