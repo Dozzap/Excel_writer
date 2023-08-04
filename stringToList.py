5dcad1b74b008955757591ea9c26d357820669f2
Dhylan Usi <78162649+Dozzap@users.noreply.github.com>
2023-08-03 20:24:19 -0400

    Minor Update: Increased number of input variables

--- stringToList.py
+++ stringToList.py
@@ -1,9 +1,10 @@
 #This will turn given string of entries into an array whilst keeping their original data type if string or int
-input1 = "9021 9022 9023 9026 9028a 9029 4510 4510 4511 4511 4512 4512 9015 22060 1438 1433 1439 1440 4512 1441 8035 4509 22060 1434 1434 1430 1436a"
-input2 = " 1437 22062 1431 12119 14609 22056 22060 14606 14606a 14607 14605 14598a 141599 14600 14602 14603 14603a"
+input1 = ""
+input2 = ""
 input3 = ""
-
-p = input1 + input2 + input3
+input4 = ""
+input5 = ""
+p = input1 + input2 + input3 input4 + input5
 
 
 def stringToList(input_string):
@@ -20,5 +21,7 @@ print(len(stringToList(p)))
 print(len(stringToList(input1)))
 print(len(stringToList(input2)))
 print(len(stringToList(input3)))
+print(len(stringToList(input4)))
+print(len(stringToList(input5)))
 print()    
 print(stringToList(p))
