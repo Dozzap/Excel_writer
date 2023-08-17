b76d804fda3de34c2f1cbc7cd0ed3430ba321bb4
Dhylan Usi <78162649+Dozzap@users.noreply.github.com>
2023-08-16 22:32:12 -0400

    Edge case fix
    Added extra edge case checker

--- stringToList.py
+++ stringToList.py
@@ -1,27 +1,37 @@
-#This will turn given string of entries into an array whilst keeping their original data type if string or int
-input1 = ""
-input2 = ""
-input3 = ""
-input4 = ""
-input5 = ""
-p = input1 + input2 + input3+ input4 + input5
+input11 = " "
+input22 = " "
+input33 = " "
+input44 = ""
+input55 = ""
+p = input11 + input22 + input33+ input44 + input55
 
 
 def stringToList(input_string):
 # Split the input string by spaces to get individual entries
+    
     entries = input_string.split()
-
+    
+    
     # Create a Python array by converting integers or keeping the original entry
-    python_array = [int(entry) if entry.isdigit() else entry for entry in entries]
+    python_array = []
+    for entry in entries:
+        if len(str(entry)) < 3:
+            print("error")
+            break
+        if entry.isdigit():
+            python_array.append(int(entry))
+        else:
+            python_array.append(entry)
+        # python_array = [int(entry) if entry.isdigit() else entry for entry in entries]
 
     # Print the resulting array
     return python_array
 
 print(len(stringToList(p)))
-print(len(stringToList(input1)))
-print(len(stringToList(input2)))
-print(len(stringToList(input3)))
-print(len(stringToList(input4)))
-print(len(stringToList(input5)))
+print(len(stringToList(input11)))
+print(len(stringToList(input22)))
+print(len(stringToList(input33)))
+print(len(stringToList(input44)))
+print(len(stringToList(input55)))
 print()    
 print(stringToList(p))
